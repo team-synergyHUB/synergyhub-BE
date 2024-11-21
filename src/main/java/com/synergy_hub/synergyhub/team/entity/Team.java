08209291f1f9e7044cca16a8288e34fd45e0d227
[CLOUD4] 유연주 <ccomi0122@gmail.com>
2024-11-21 11:28:36 +0900

    feat: team 엔티티 생성

--- src/main/java/com/synergy_hub/synergyhub/team/entity/Team.java
+++ src/main/java/com/synergy_hub/synergyhub/team/entity/Team.java
@@ -3,6 +3,7 @@ package com.synergy_hub.synergyhub.team.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
 
 @Getter
 @NoArgsConstructor
@@ -14,14 +15,19 @@ public class Team {
     private Long id;
 
     @Column(nullable = false, length = 255)
-    private String name;
+    private String name; // 팀 이름
 
     @Column(nullable = false, length = 8)
-    private String inviteCode;
+    private String inviteCode; // 초대 코드
 
-    @Column(nullable = false, length = 255)
-    private String inviteSecret;
+    @Column(nullable = false, length = 30)
+    private String inviteSecret; // 초대 비밀번호
 
     @Column(nullable = false)
-    private Boolean isDeleted = false;
+    private Boolean isDeleted = false; // 삭제 여부
+
+    @ManyToOne
+    @JoinColumn(name = "label_id", nullable = true)
+    private Label label; // 팀과 연결된 라벨
 }
+
