2d143dd4d2e7ba1f9b5e2b444bb1e34f1b313a2d
[CLOUD4] 유연주 <ccomi0122@gmail.com>
2024-11-21 11:23:49 +0900

    feat: team 엔티티 생성

--- src/main/java/com/synergy_hub/synergyhub/team/entity/Team.java
+++ src/main/java/com/synergy_hub/synergyhub/team/entity/Team.java
@@ -1,36 +1,27 @@
 package com.synergy_hub.synergyhub.team.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@Entity
+@Table(name = "team")
 public class Team {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "team_id", nullable = false, updatable = false)
-    private Long id; // 팀 ID
+    private Long id;
 
-    @Column(name = "name", nullable = false, length = 255)
-    private String name; // 팀 이름
+    @Column(nullable = false, length = 255)
+    private String name;
 
-    @Column(name = "invite_code", nullable = false, length = 8, unique = true)
-    private String inviteCode; // 초대 코드
+    @Column(nullable = false, length = 8)
+    private String inviteCode;
 
-    @Column(name = "invite_secret", nullable = false, length = 255)
-    private String inviteSecret; // 초대 비밀번호
+    @Column(nullable = false, length = 255)
+    private String inviteSecret;
 
-    @Column(name = "is_deleted", nullable = false)
-    private Boolean isDeleted; // 팀 삭제 여부 (소프트 삭제)
-
-    @Column(name = "labels", columnDefinition = "JSON")
-    private String labels; // JSON 형식의 라벨
+    @Column(nullable = false)
+    private Boolean isDeleted = false;
 }
-
