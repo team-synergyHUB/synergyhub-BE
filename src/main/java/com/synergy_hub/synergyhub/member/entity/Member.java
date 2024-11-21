36e279d996ae13710efc3ce8bd998c17670edcec
[CLOUD4] 심우민 <worejj@naver.com>
2024-11-21 16:26:31 +0900

    feat: member, memberTeam 연관관계 설정

--- src/main/java/com/synergy_hub/synergyhub/member/entity/Member.java
+++ src/main/java/com/synergy_hub/synergyhub/member/entity/Member.java
@@ -1,10 +1,14 @@
 package com.synergy_hub.synergyhub.member.entity;
 
+import com.synergy_hub.synergyhub.team.entity.MemberTeam;
+import com.synergy_hub.synergyhub.team.entity.Team;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -27,6 +31,22 @@ public class Member {
 
     @Column(nullable = false)
     private String password;
+
+    @OneToMany(mappedBy = "member")
+    private List<MemberTeam> memberTeams;
+
+    //특정 팀에 참여
+    public void joinTeam(Team team) {
+
+        MemberTeam memberTeam = new MemberTeam();
+//        memberTeam.setTeamAndMember(team, this);
+
+    }
+
+    //연관 관계 편의 메서드
+    public void addMemberTeams(MemberTeam memberTeam) {
+        this.memberTeams.add(memberTeam);
+    }
 
     private Member(String nickname, String email, String password) {
         this.nickname = nickname;
