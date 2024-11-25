95150e48920bb1e09c609b741038d92972128e21
[CLOUD4] 이하현 <lovehh329@naver.com>
2024-11-25 15:45:18 +0900

    fix: chat, chatRoom 컨트롤러, 서비스만

--- src/main/java/com/synergy_hub/synergyhub/chat/repository/MessageRepository.java
+++ src/main/java/com/synergy_hub/synergyhub/chat/repository/MessageRepository.java
@@ -1,7 +1,6 @@
 package com.synergy_hub.synergyhub.chat.repository;
 
 import com.synergy_hub.synergyhub.chat.entity.ChatMessage;
-import com.synergy_hub.synergyhub.chat.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
