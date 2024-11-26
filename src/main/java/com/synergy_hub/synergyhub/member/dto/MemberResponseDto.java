2d1c458e2def36404a78ed34b24dc16d24dc66f5
[CLOUD4] 심우민 <worejj@naver.com>
2024-11-26 16:46:51 +0900

    Feat: 회원 조회 기능 개발

---
+++ src/main/java/com/synergy_hub/synergyhub/member/dto/MemberResponseDto.java
@@ -0,0 +1,18 @@
+package com.synergy_hub.synergyhub.member.dto;
+
+import com.synergy_hub.synergyhub.member.entity.Member;
+import lombok.Data;
+
+@Data
+public class MemberResponseDto {
+
+    private Long id;
+    private String nickname;
+    private String email;
+
+    public MemberResponseDto(Member member) {
+        this.id = member.getId();
+        this.nickname = member.getNickname();
+        this.email = member.getEmail();
+    }
+}
