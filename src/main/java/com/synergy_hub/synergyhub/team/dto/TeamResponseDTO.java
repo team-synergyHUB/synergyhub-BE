8dd0e2070dcc2d0e4be13cdc5d14cab227bb1076
[CLOUD4] 유연주 <ccomi0122@gmail.com>
2024-12-02 13:25:46 +0900

    refactor: teamLabel 수정(팀,라벨 매핑)

--- src/main/java/com/synergy_hub/synergyhub/team/dto/TeamResponseDTO.java
+++ src/main/java/com/synergy_hub/synergyhub/team/dto/TeamResponseDTO.java
@@ -1,7 +1,5 @@
 package com.synergy_hub.synergyhub.team.dto;
 
-import com.synergy_hub.synergyhub.calendar.entity.Calendar;
-import com.synergy_hub.synergyhub.chat.entity.ChatRoom;
 import com.synergy_hub.synergyhub.team.entity.Team;
 import lombok.Data;
 
@@ -13,7 +11,7 @@ public class TeamResponseDTO {
     private Long id; // 팀 ID
     private String name; // 팀 이름
     private String inviteCode; // 초대 코드
-    private List<LabelDTO> labels; // 라벨 리스트
+    private List<LabelRequestDTO> labels; // 라벨 리스트
 
     // 엔티티를 DTO로 변환하는 생성자
     public TeamResponseDTO(Team team) {
@@ -21,7 +19,7 @@ public class TeamResponseDTO {
         this.name = team.getName();
         this.inviteCode = team.getInviteCode();
         this.labels = team.getLabels().stream()
-                .map(LabelDTO::new) // 라벨 엔티티를 DTO로 변환
+                .map(LabelRequestDTO::new) // 라벨 엔티티를 DTO로 변환
                 .collect(Collectors.toList());
     }
 }
