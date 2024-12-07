c602fbdcb23de37745a2e64aa4024d00c9d33614
[CLOUD4] 임서현 <violethart123@gmail.com>
2024-12-08 00:11:53 +0900

    refactor - 댓글 수정

--- src/main/java/com/synergy_hub/synergyhub/comment/controller/CommentController.java
+++ src/main/java/com/synergy_hub/synergyhub/comment/controller/CommentController.java
@@ -3,6 +3,11 @@ package com.synergy_hub.synergyhub.comment.controller;
 import com.synergy_hub.synergyhub.comment.dto.CommentRequestDto;
 import com.synergy_hub.synergyhub.comment.dto.CommentResponseDto;
 import com.synergy_hub.synergyhub.comment.service.CommentService;
+import com.synergy_hub.synergyhub.global.exception.CustomException;
+import com.synergy_hub.synergyhub.global.exception.ErrorCode;
+import com.synergy_hub.synergyhub.member.controller.MemberController;
+import com.synergy_hub.synergyhub.member.entity.Member;
+import com.synergy_hub.synergyhub.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,13 +20,19 @@ import java.util.List;
 public class CommentController {
 
     private final CommentService commentService;
+    private final MemberRepository memberRepository;
 
     // 댓글 생성
     @PostMapping
-    public ResponseEntity<CommentResponseDto> createComment(@RequestBody CommentRequestDto dto) {
-        CommentResponseDto createdComment = commentService.createComment(dto);
+    public ResponseEntity<CommentResponseDto> createComment(@RequestBody CommentRequestDto dto, @RequestParam("team") Long teamId) {
+        // 현재 인증된 사용자 ID 가져오기
+        Long currentMemberId = MemberController.getAuthenticationMemberId();
+
+        // 댓글 생성 시 회원 정보와 팀 ID 전달
+        CommentResponseDto createdComment = commentService.createComment(dto, currentMemberId, teamId);
         return ResponseEntity.ok(createdComment);
     }
+
 
     // 댓글 조회 (특정 공지사항 기준)
     @GetMapping("/notice/{noticeId}")
@@ -46,4 +57,3 @@ public class CommentController {
         return ResponseEntity.noContent().build();
     }
 }
-
