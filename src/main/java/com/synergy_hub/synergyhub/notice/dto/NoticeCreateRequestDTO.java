064a7edc73a929930698f10e001d0bca4d597826
[CLOUD4] 임서현 <violethart123@gmail.com>
2024-12-10 04:01:11 +0900

    refactor:캘린더 팀 공지사항 멤버 댓글 @schema 적용

--- src/main/java/com/synergy_hub/synergyhub/notice/dto/NoticeCreateRequestDTO.java
+++ src/main/java/com/synergy_hub/synergyhub/notice/dto/NoticeCreateRequestDTO.java
@@ -1,5 +1,6 @@
 package com.synergy_hub.synergyhub.notice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,12 +9,15 @@ import lombok.Setter;
 @Getter
 public class NoticeCreateRequestDTO {
 
+    @Schema(description = "공지사항 제목" ,example = "팀 회의 공지", required = true )
     @NotBlank(message = "제목을 입력해주세요.")
     private String title;
 
+    @Schema(description = "공지사항 내용", example = "다음 주 화요일 오후 3시에 팀 회의가 있습니다.", required = true)
     @NotBlank(message = "내용을 입력해주세요.")
     private String content;
 
+    @Schema(description = "이미지 파일의 URL", example = "https://example.com/images/notice1.png", required = false)
     private String imageUrl;
 
 //    private Long teamId;
--- src/main/java/com/synergy_hub/synergyhub/team/dto/LabelRequestDTO.java
+++ src/main/java/com/synergy_hub/synergyhub/team/dto/LabelRequestDTO.java
@@ -1,5 +1,6 @@
 package com.synergy_hub.synergyhub.team.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import com.synergy_hub.synergyhub.team.entity.Label;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -7,8 +8,12 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class LabelRequestDTO {
-    private String name; // 라벨 이름
-    private String color; // 라벨 색상
+
+    @Schema(description = "라벨 이름", example = "abcd")
+    private String name;
+
+    @Schema(description = "라벨 색상 코드", example = "#FF5733")
+    private String color;
 
     // 라벨 엔티티를 DTO로 변환하는 생성자
     public LabelRequestDTO(Label label) {
@@ -16,4 +21,3 @@ public class LabelRequestDTO {
         this.color = label.getColor();
     }
 }
-
